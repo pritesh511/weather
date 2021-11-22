644bd9db7c21aae4f0ae59e8d897db66f617b4c2
pritesh511 <patelpritesh313@gmail.com>
2021-11-22 09:26:40 +0530

    all setup ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Container from "./components/Container/Container";
 
-function App() {
+const AppWrapper = styled.div`
+height: 97vh;
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: aliceblue;
+`;
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Container/>
+    </AppWrapper>
   );
 }
 
---
+++ src/components/Container/Container.js
@@ -0,0 +1,11 @@
+import {MainWrapper} from './styles'
+
+const Container = () =>{
+    return(
+        <>
+        <MainWrapper>Hello</MainWrapper>
+        </>
+    )
+};
+
+export default Container;
